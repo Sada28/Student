b1cb4ab64cf385edfce736911fea4f8305afa1ac
Sumit Madame <sadanandm1234@gmail.com>
2020-08-20 12:43:19 +0530

    changes done by sada

--- app/src/main/java/com/example/student/Login.java
+++ app/src/main/java/com/example/student/Login.java
@@ -2,13 +2,27 @@ package com.example.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView tvAc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvAc=findViewById(R.id.tvAc);
+        tvAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Login.this,Signup.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
